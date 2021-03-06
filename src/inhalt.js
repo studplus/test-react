dcb637b05d7741c2eefcf54dc526da212539193f
Mathias <mathias@studierenplus.de>
2021-03-06 17:34:47 +0100

    liken von componenten

--- src/inhalt.js
+++ src/inhalt.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import{ dumpLogs } from "./Utils";
 import classes from "./inhalt.module.css";
 
-const Inhalt = (props) => {
-    dumpLogs(props)
-    return(
-        <div className={classes.TestClass}>
-            <h3>{props.title}</h3>
-            <p>{props.beschreibung}</p>
-        </div>
-    )
+class Inhalt extends Component {
+    state = {
+        anzahlLikes: 0
+    }
+
+    onLikeBtnClick = () => {
+        this.setState((prevState, prevProp)=> {
+          return {anzahlLikes: prevState.anzahlLikes + 1}
+        });
+    }
+
+    render(){
+        dumpLogs(this.props)
+        return(
+            <div className={classes.TestClass}>
+                <h3>{this.props.title}</h3>
+                <p>{this.props.beschreibung}</p>
+
+                <p>Anzahl Likes: <span className={classes.AnzahlLikes}>{this.state.anzahlLikes}</span></p>
+                <button onClick={this.onLikeBtnClick}>Like</button>
+            </div>
+        )
+    }
 }
 
 export default Inhalt;
